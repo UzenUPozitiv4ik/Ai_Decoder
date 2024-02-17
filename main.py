164b6c6a5a7be2062f8051aeede824e0e9a940d6
UzenUPozitiv4ik <160276512+UzenUPozitiv4ik@users.noreply.github.com>
2024-02-17 17:55:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,23 @@
+from tkinter import *
+from tkinter import messagebox
 import os
 from openai import OpenAI
+import pyperclip
 
+#Выводит лист, как текст
+def text_normalization(ans):
+    answer=str(ans)
+    answer1=""
+    for i in answer:
+        if i == "'" or i == "[" or i == "]":
+            continue
+        if i == ",":
+            answer1+="."
+            continue
+        answer1+=i
+    return answer1
 
-
-#Загрузка api ключа для работы ChatGpt 3.5 turbo
-client = OpenAI(
-    api_key="Ваш-api-key"
-)
-def src(text):
-    response = client.chat.completions.create(
-        messages=[
-           {
-                "role": "user",
-                "content": text,
-            }
-        ],
-        model="gpt-3.5-turbo",
-    )
-    return response.choices[0].message.content
-
-
-text=input()
-
-
-#распознование языка
-language=26
-ABC="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ                                                                                                                                          "
-ABC_Atbash="zyxwvutsrqponmlkjihgfedcbazyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBAZYXWVUTSRQPONMLKJIHGFEDCBA      "
-if "р" in src("Какой алфавит используется в следующем предложении"+text+".В ответе напиши только название"):
-    language=33
-    ABC="абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ                                                                                                                                          "
-    ABC_Atbash="яюэьыъщшчцхфутсрпонмлкйизжёедгвбаяюэьыъщшчцхфутсрпонмлкйизжёедгвбаЯЮЭЬЫЪЩШЧЦХФУТСРПОНМЛКЙИЗЖЁЕДГВБАЯЮЭЬЫЪЩШЧЦХФУТСРПОНМЛКЙИЗЖЁЕДГВБА          "
-ans=[]
-
-
-#Шифр цезаря
-for i in range(language):
-    new_text=""
-    for j in text:
-        f=ABC.find(j)
-        new_text+=(ABC[f+i])
-    ans.append(new_text)
-
-
-# Шифр Атбаш
-Atbash=""
-for i in text:
-    Atbash+=ABC_Atbash[ABC.find(i)]
-ans.append(Atbash)
-
-
-#Вывод ответов
-answer=str(ans)
-answer1=""
-for i in answer:
-    if i == "'" or i == "[" or i == "]":
-        continue
-    if i == ",":
-        answer1+="."
-        continue
-    answer1+=i
-print("Возможные расшифровки")
-print(answer1)
-answer1=src("Есть ли в следующих предлжениях, такое предложение, что его смысл понятен человеку? "+answer1)
-print(answer1)
-
-
-if input("Вы довольны результатом? y/n - ") == "y":
-    exit()
-
-
-#Шифр Гронсфельда 
-#длина ключа <= 6
-answer=[]
-
+#Расшифровка шифра Гронсфельда при заданном ключе
 def decrypt_gronsfeld(key):
     global answer
     new_text=""
@@ -86,8 +30,10 @@ def decrypt_gronsfeld(key):
         counter+=1
     answer.append(new_text)
 
+#рекурсивный перебор для шифра Гронсфельда
 def recursive_combinations(length):
-    if length == 6:
+    global key
+    if length == 3:
         return
     for i in range(10):
         key.append(i)
@@ -95,23 +41,83 @@ def recursive_combinations(length):
         recursive_combinations(length+1)
         key.pop()
 
+def paste_api():
+    h_tf.insert(0, pyperclip.paste)
 
-key=[]
-for i in range(1,10):
-    key.append(i)
-    recursive_combinations(1)
-    key.pop()
+def paste_text():
+    w_tf.insert(0, pyperclip.paste)
+
+def main():
+    global answer, key,text,ABC
+    api = h_tf.get()
+    text = w_tf.get()
+    #Загрузка api ключа для работы ChatGpt 3.5 turbo
+    client = OpenAI(api_key=api)
+    def src(text):
+        response = client.chat.completions.create(
+            messages=[
+               {
+                    "role": "user",
+                    "content": text,
+                }
+            ],
+            model="gpt-3.5-turbo",
+        )
+        return response.choices[0].message.content
+    #распознование языка
+    language=26
+    ABC="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ                                                                                                                                          "
+    ABC_Atbash="zyxwvutsrqponmlkjihgfedcbazyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBAZYXWVUTSRQPONMLKJIHGFEDCBA      "
+    if "р" in src("Какой алфавит используется в следующем предложении"+text+".В ответе напиши только название"):
+        language=33
+        ABC="абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ                                                                                                                                          "
+        ABC_Atbash="яюэьыъщшчцхфутсрпонмлкйизжёедгвбаяюэьыъщшчцхфутсрпонмлкйизжёедгвбаЯЮЭЬЫЪЩШЧЦХФУТСРПОНМЛКЙИЗЖЁЕДГВБАЯЮЭЬЫЪЩШЧЦХФУТСРПОНМЛКЙИЗЖЁЕДГВБА          "
+    ans=[]
 
 
-answer=str(ans)
-answer1=""
-for i in answer:
-    if i == "'" or i == "[" or i == "]":
-        continue
-    if i == ",":
-        answer1+="."
-        continue
-    answer1+=i
-print(answer1)
-answer1=src("Есть ли в следующих предлжениях, такое предложение, что его смысл понятен человеку? "+answer1)
-print(answer1)
+    #Шифр цезаря
+    for i in range(language):
+        new_text=""
+        for j in text:
+            f=ABC.find(j)
+            new_text+=(ABC[f+i])
+        ans.append(new_text)
+
+
+    # Шифр Атбаш
+    Atbash=""
+    for i in text:
+        Atbash+=ABC_Atbash[ABC.find(i)]
+    ans.append(Atbash)
+
+
+    #Вывод ответов
+    print(text_normalization(ans))
+    if "yes" != messagebox.askquestion("Дешифратор с использованием ИИ", "Вы довольны следующему результату?"+"\n"+src("Есть ли в следующих предлжениях, такое предложение, что его смысл понятен человеку? "+text_normalization(ans))):
+        answer=[]
+        key=[]
+        for i in range(10):
+            key.append(i)
+            recursive_combinations(1)
+            key.pop()
+        messagebox.showinfo("Дешифратор с использованием ИИ", src("Есть ли в следующих предлжениях, такое предложение, что его смысл понятен человеку? "+text_normalization(answer)))
+
+#для интерфейса
+window = Tk()
+window.title('Дешифратор с использованием ИИ')
+window.geometry('400x300')
+frame = Frame(window,padx=10,pady=10)
+frame.pack(expand=True)
+h_lb = Label(frame,text="Введите api key")
+h_lb.grid(row=3, column=1)
+w_lb = Label(frame,text="Введите зашифрованный текст ",)
+w_lb.grid(row=4, column=1)
+h_tf = Entry(frame,)
+h_tf.grid(row=3, column=2, pady=5)
+w_tf = Entry(frame,)
+w_tf.grid(row=4, column=2, pady=5)
+cal_btn = Button(frame,text='Расшифровать',command=main)
+cal_btn.grid(row=5, column=2)
+btn = Button(text='Вставить api ключ из буфера', command=paste_api).pack()
+btn = Button(text='Вставить текст из буфера', command=paste_text).pack()
+window.mainloop()
